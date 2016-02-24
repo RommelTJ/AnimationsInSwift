3e343919a09be3757434314dbcd926da44600204
Rommel Rico <rommeltj@gmail.com>
2016-02-24 15:37:32 -0800

    Added a button to start and stop animations.

--- AnimationsInSwift/ViewController.swift
+++ AnimationsInSwift/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     var squareSide: CGFloat = 50
     var viewWidth: CGFloat = 0
     var viewHeight: CGFloat = 0
+    var squareView: UIView!
+    var running = false
+    @IBOutlet weak var animateButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,49 +25,39 @@ class ViewController: UIViewController {
         viewHeight = self.view.frame.height
         
         //Create and add a red square.
-        let square = UIView()
-        square.backgroundColor = UIColor.redColor()
-        square.frame = CGRect(x: 0, y: 120, width: squareSide, height: squareSide)
-        self.view.addSubview(square)
-        
-        //Animate the square to the other side of the screen in 1 second
-        /*
-        UIView.animateWithDuration(1.0) { () -> Void in
-            square.backgroundColor = UIColor.orangeColor()
-            square.frame = CGRect(x: self.viewWidth - self.squareSide, y: 120, width: self.squareSide, height: self.squareSide)
-        }
-        */
-        
-        //UIViewAnimationOptions - Repeating the animation.
-        /*
-        let options = UIViewAnimationOptions.Repeat
-        UIView.animateWithDuration(1.0, delay: 0.0, options: options, animations: { () -> Void in
-            square.backgroundColor = UIColor.orangeColor()
-            square.frame = CGRect(x: self.viewWidth - self.squareSide, y: 120, width: self.squareSide, height: self.squareSide)
-            }, completion: nil)
-        */
-        
-        //UIViewAnimationOptions - Autoreversing the animation.
-        /*
-        let options = UIViewAnimationOptions.Autoreverse
-        UIView.animateWithDuration(1.0, delay: 0.0, options: options, animations: { () -> Void in
-            square.backgroundColor = UIColor.orangeColor()
-            square.frame = CGRect(x: self.viewWidth - self.squareSide, y: 120, width: self.squareSide, height: self.squareSide)
-            }, completion: nil)
-        */
-        
-        //UIViewAnimationOptions - Combining multiple animations in Swift 2.0
-        let options: UIViewAnimationOptions = [UIViewAnimationOptions.Autoreverse, UIViewAnimationOptions.Repeat, UIViewAnimationOptions.CurveEaseInOut]
-        UIView.animateWithDuration(1.0, delay: 0.0, options: options, animations: { () -> Void in
-        square.backgroundColor = UIColor.orangeColor()
-        square.frame = CGRect(x: self.viewWidth - self.squareSide, y: 120, width: self.squareSide, height: self.squareSide)
-        }, completion: nil)
+        squareView = UIView()
+        squareView.backgroundColor = UIColor.redColor()
+        squareView.frame = CGRect(x: 0, y: 120, width: squareSide, height: squareSide)
+        self.view.addSubview(squareView)
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func animateSquare(sender: AnyObject) {
+        if (running) {
+            //Animate the square back to the starting position.
+            UIView.animateWithDuration(1.0, animations: { () -> Void in
+                self.squareView.backgroundColor = UIColor.redColor()
+                self.squareView.frame = CGRect(x: 0, y: 120, width: self.squareSide, height: self.squareSide)
+            })
+            self.running = false
+            self.animateButton.setTitle("Animate", forState: .Normal)
+        } else {
+            //UIViewAnimationOptions - Combining multiple animations in Swift 2.0
+            let options: UIViewAnimationOptions = [UIViewAnimationOptions.Autoreverse, UIViewAnimationOptions.Repeat, UIViewAnimationOptions.CurveEaseInOut]
+            UIView.animateWithDuration(1.0, delay: 0.0, options: options, animations: { () -> Void in
+                self.squareView.backgroundColor = UIColor.orangeColor()
+                self.squareView.frame = CGRect(x: self.viewWidth - self.squareSide, y: 120, width: self.squareSide, height: self.squareSide)
+                }, completion: nil)
+            
+            self.running = true
+            self.animateButton.setTitle("Stop", forState: .Normal)
+        }
+        
     }
 
 
