584bf7cadb4faf5594b2f9b2dd82406532c4169c
Rommel Rico <rommeltj@gmail.com>
2016-02-29 08:21:33 -0800

    Curl up animation

--- AnimationsInSwift/MoreAnimationsViewController.swift
+++ AnimationsInSwift/MoreAnimationsViewController.swift
@@ -9,14 +9,57 @@
 import UIKit
 
 class MoreAnimationsViewController: UIViewController {
-
+    //Properties
+    var viewWidth: CGFloat = 0
+    var viewHeight: CGFloat = 0
+    let container = UIView()
+    let redSquare = UIView()
+    let blueSquare = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("This ran")
+        
+        //Set the viewWidth and Height
+        viewWidth = self.view.frame.width
+        viewHeight = self.view.frame.height
+        
+        //Set Container and add it to the screen.
+        self.container.frame = CGRect(x: (viewWidth/2) - (200/2), y: 60, width: 200, height: 200)
+        self.view.addSubview(container)
+        
+        //Set Red square frame. We want the blue square to have the same 
+        //position as the red square, so let's just reuse blueSquare.frame
+        self.redSquare.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        self.blueSquare.frame = redSquare.frame
+        
+        //Set the background colors.
+        self.redSquare.backgroundColor = UIColor.redColor()
+        self.blueSquare.backgroundColor = UIColor.blueColor()
+        
+        //Add the squares.
+        self.container.addSubview(self.redSquare)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func doAnimate(sender: AnyObject) {
+        //Create a tuple of views.
+        let views = (frontView: self.redSquare, backView: self.blueSquare)
+        
+        //Set a transition style.
+        let transitionOptions = UIViewAnimationOptions.TransitionCurlUp
+        
+        //Animation
+        UIView.transitionWithView(self.container, duration: 1.0, options: transitionOptions, animations: { () -> Void in
+            //Remove the front object.
+            views.frontView.removeFromSuperview()
+            //Then Add the other object.
+            self.container.addSubview(views.backView)
+            }) { (finished) -> Void in
+                print("Finished")
+        }
     }
 
 }
