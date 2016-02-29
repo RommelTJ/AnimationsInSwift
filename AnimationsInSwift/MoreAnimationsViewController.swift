fe99420160eb9596d2514d4d377b1845744a82c1
Rommel Rico <rommeltj@gmail.com>
2016-02-29 15:12:24 -0800

    Refactoring so we can add more animations

--- AnimationsInSwift/MoreAnimationsViewController.swift
+++ AnimationsInSwift/MoreAnimationsViewController.swift
@@ -12,6 +12,7 @@ class MoreAnimationsViewController: UIViewController {
     //Properties
     var viewWidth: CGFloat = 0
     var viewHeight: CGFloat = 0
+    var containerSetup = false
     let container = UIView()
     let redSquare = UIView()
     let blueSquare = UIView()
@@ -22,12 +23,27 @@ class MoreAnimationsViewController: UIViewController {
         //Set the viewWidth and Height
         viewWidth = self.view.frame.width
         viewHeight = self.view.frame.height
-        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func doAnimate(sender: AnyObject) {
+        if (containerSetup == false) {
+            setContainerView()
+            containerAnimate()
+        } else {
+            containerAnimate()
+        }
+    }
+
+    func setContainerView() {
         //Set Container and add it to the screen.
         self.container.frame = CGRect(x: (viewWidth/2) - (200/2), y: 60, width: 200, height: 200)
         self.view.addSubview(container)
         
-        //Set Red square frame. We want the blue square to have the same 
+        //Set Red square frame. We want the blue square to have the same
         //position as the red square, so let's just reuse blueSquare.frame
         self.redSquare.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
         self.blueSquare.frame = redSquare.frame
@@ -38,13 +54,12 @@ class MoreAnimationsViewController: UIViewController {
         
         //Add the squares.
         self.container.addSubview(self.redSquare)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        
+        //Mark as setup
+        containerSetup = true
     }
     
-    @IBAction func doAnimate(sender: AnyObject) {
+    func containerAnimate() {
         //Create a tuple of views.
         var views : (frontView: UIView, backView: UIView)
         
@@ -58,22 +73,21 @@ class MoreAnimationsViewController: UIViewController {
         }
         
         //Set a transition style.
-        //let transitionOptions = UIViewAnimationOptions.TransitionCurlUp
+        let transitionOptions = UIViewAnimationOptions.TransitionCurlUp
         //let transitionOptions = UIViewAnimationOptions.TransitionCurlDown
-        let transitionOptions = UIViewAnimationOptions.TransitionFlipFromLeft
+        //let transitionOptions = UIViewAnimationOptions.TransitionFlipFromLeft
         
         //Animation
         /*
         UIView.transitionWithView(self.container, duration: 1.0, options: transitionOptions, animations: { () -> Void in
-            //Remove the front object.
-            views.frontView.removeFromSuperview()
-            //Then Add the other object.
-            self.container.addSubview(views.backView)
-            }) { (finished) -> Void in
-                print("Finished")
+        //Remove the front object.
+        views.frontView.removeFromSuperview()
+        //Then Add the other object.
+        self.container.addSubview(views.backView)
+        }) { (finished) -> Void in
+        print("Finished")
         }
         */
         UIView.transitionFromView(views.frontView, toView: views.backView, duration: 1.0, options: transitionOptions, completion: nil)
     }
-
 }
